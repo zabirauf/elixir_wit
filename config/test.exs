d4a1c597cce15838da3643f167fdf3bb0dd8f9d0
Micah Wedemeyer <me@micahwedemeyer.com>
2017-03-10 10:05:32 -0500

    Manage test access tokens in Mix config

---
+++ config/test.exs
@@ -0,0 +1,8 @@
+use Mix.Config
+
+config :elixir_wit, :wit_credentials,
+  app_id: "put it in test.secret.exs",
+  server_access_token: "put it in test.secret.exs",
+  client_access_token: "put it in test.secret.exs"
+
+import_config "#{Mix.env}.secret.exs"

edb85a1a44c99ee2a6443b76774c6b6f7c05cb11
Micah Wedemeyer <me@micahwedemeyer.com>
2017-03-13 15:52:53 -0400

    Travis CI updates

---
+++ config/docs.exs
@@ -0,0 +1,6 @@
+use Mix.Config
+
+config :elixir_wit, :wit_credentials,
+  app_id: "put it in test.secret.exs",
+  server_access_token: "put it in test.secret.exs",
+  client_access_token: "put it in test.secret.exs"
